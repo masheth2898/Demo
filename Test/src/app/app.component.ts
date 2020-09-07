aed6ccddf1f4ca2478332879a2241a390fb8a980
Mitul Sheth <masheth2898@gmail.com>
2020-09-07 14:34:58 +0530

    Binding in Angular
    Implmented different type of binding in angular.

--- Test/src/app/app.component.ts
+++ Test/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'Test';
+export class AppComponent implements AfterViewInit {
+
+pageHeader = 'Angular Components Interaction';
+count=0;
+name:string;
+userName:string;
+private _customerName:string;
+imageUrl='https://picsum.photos/100';
+
+
+@ViewChild('nameRef',{static:true}) nameElementRef:ElementRef;
+
+ngAfterViewInit(): void {
+  this.nameElementRef.nativeElement.focus();
+}
+
+clickEvent(){
+  this.count+=1;
+}
+get customerName(){
+  return this._customerName;
+}
+
+set customerName(value:string){
+  this._customerName = value;
+
+  if(value === "Mitul"){
+    alert('Hello Mitul.');
+  }
+}
+greetUser(updateValue){
+  this.userName = updateValue;
+  if(updateValue === "Mitul"){
+    alert('Welcome back Mitul.');
+  }
+}
 }
